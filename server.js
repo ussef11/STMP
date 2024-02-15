9e146cb8d51cd78eaef6f94567364e16c6c980ed
khiraouiyoussef@gmail.com <khiraouiyoussef@gmail.com>
2024-02-15 09:52:58 +0100

    update By ussef

---
+++ server.js
@@ -0,0 +1,132 @@
+// const express = require("express");
+// const app = express();
+// const port = 5001;
+// const { createTransport } = require("nodemailer");
+
+// const transporter = createTransport({
+//   host: "smtp-relay.brevo.com",
+//   port: 587,
+//   auth: {
+//     user: "gabrieleduera@gmail.com",
+//     pass: "g71FMp6aJymt24LB",
+//   },
+// });
+
+// const mailOptions = {
+//   from: "gabrieleduera@gmail.com",
+//   to: "youssefkhiraoui15@gmail.com",
+//   subject: `Your subject`,
+//   text: `Your text content`,
+// };
+
+// app.get("/", (req, res) => {
+//   const recipient = [
+//     "belkhirayoussef770@gmail.com",
+//     "youssefkhiraoui15@gmail.com",
+//   ];
+//   recipient.forEach((element, index) => {
+//     const mailOptions = {
+//       from: "gabrieleduera@gmail.com",
+//       to: element,
+//       subject: `Your subject`,
+//       html: `<p>This is your HTML content for recipient ${
+//         index + 1
+//       }</p><p>More HTML content...</p>`,
+//     };
+
+//     for (let i = 0; i < 10; i++) {
+//       transporter.sendMail(mailOptions, function (error, info) {
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log("Email sent: " + info.response);
+//         }
+//       });
+//     }
+
+//     if (index === recipient.length - 1) {
+//       res.send("Emails sent successfully");
+//     }
+//   });
+
+//   // res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const server = http.createServer(app);
+const fs = require("fs");
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "public")));
+
+let activeClients = 0;
+
+io.on("connection", socket => {
+  console.log("A user connected");
+  activeClients++;
+  
+  io.emit("activeClients", activeClients);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    activeClients--;
+    io.emit("activeClients", activeClients);
+  });
+
+  socket.on("stream", data => {
+    io.emit("stream", data);
+  });
+
+  socket.on("capturedImage", data => {
+    console.log("Received capturedImage event:", data);
+    io.emit("capturedImage", data);
+  });
+
+  
+  
+  socket.on("randomValue", randomValue => {
+    io.emit("randomValue", randomValue);
+    console.log("Random Value:", randomValue);
+  });
+
+
+
+  socket.on("toggleStream" , isStream  =>{
+    io.emit("toggleStream" , isStream);
+    console.log('toggleStream' , isStream)
+  })
+
+});
+
+
+
+
+
+
+const PORT = 5002;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
