036066c9f4a1bf14af45ff0f2e84159c5bbddbaf
khiraouiyoussef@gmail.com <khiraouiyoussef@gmail.com>
2024-02-20 11:55:03 +0100

    update by ussef

--- server.js
+++ server.js
@@ -64,6 +64,42 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const fs = require("fs");
+const multer = require('multer');
+
+
+require("dotenv").config({
+  override: true,
+  path: path.join(__dirname, ".env"),
+});
+
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+(async () => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await client.query("SELECT current_user");
+    const currentUser = rows[0]["current_user"];
+    console.log(currentUser);
+  } catch (err) {
+    console.error("Error executing query:", err);
+  } finally {
+    client.release();
+  }
+})();
+
+
+
+
+
+
 
 const io = socketIO(server, {
   cors: {
@@ -81,6 +117,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 let activeClients = 0;
 
@@ -112,10 +149,24 @@ io.on("connection", socket => {
     console.log("Random Value:", randomValue);
   });
 
+  
+  
+  socket.on("history", History => {
+    io.emit("history", History);
+    console.log("History Value:", History);
+  });
+
+  
+  socket.on("GetVideo", GetVideo => {
+    io.emit("GetVideo", GetVideo);
+    console.log("GetVideo Value:", GetVideo);
+  });
 
 
   socket.on("toggleStream" , isStream  =>{
     io.emit("toggleStream" , isStream);
+
+    _toggleValueStream(isStream.isStream , isStream.device)
     console.log('toggleStream' , isStream)
   })
 
@@ -123,6 +174,68 @@ io.on("connection", socket => {
 
 
 
+
+app.post("/api/status", async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ success: false, message: "'name' property is missing in the request body" });
+    }
+
+    const insertQuery = ` SELECT stream FROM public."NFC" where  nom = $1`;
+    const value = [name];
+
+    const result = await pool.query(insertQuery, value);
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error("Error executing query:", err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+
+
+
+const _toggleValueStream = async (value , device) => {
+  const selectQuery = 'update public."NFC"  set stream  = $1  where  nom = $2';
+  const values = [value ,device];
+  const result = await pool.query(selectQuery, values);
+  return result;
+};
+
+
+
+
+
+const videoStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const videoUpload = multer({
+  storage: videoStorage,
+  limits: {
+  fileSize: 10000000 // 10000000 Bytes = 10 MB
+  }
+})
+
+app.post('/upload', videoUpload.single('video'), (req, res) => {
+  res.send(req.file)
+
+}, (error, req, res, next) => {
+   res.status(400).send({ error: error.message })
+})
+
+
+
+
+
+app.use('/videos', express.static(path.join(__dirname, 'uploads')));
 
 
 
